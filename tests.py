349b48e9aa9b9c5f10bfb877373aa686da8156c3
Harriet Clerry <clerryha@yahoo.com>
2017-11-05 14:38:43 +0300

    tested my project

---
+++ tests.py
@@ -0,0 +1,74 @@
+import unittest
+from models import User,Category,Recipe
+
+class UserTest(unittest.TestCase):
+    def setUp(self):
+        self.user = User("Harriet", "har@gmail.com", "password")
+
+   
+    def test_add_category(self):
+        self.assertEqual( self.user.add_category("dinner"),
+                        True)
+
+    def test_add_category_name_already_exists(self):
+        self.user.add_category("dinner")
+        self.assertEqual( self.user.add_category
+                         ("dinner"),
+                         False)
+
+
+    def test_edit_category_not_found(self):
+        self.assertEqual( self.user.edit_category("absent", "newtype"),
+                         False)
+
+    def test_edit_category_successful(self):
+        self.user.add_category("Snacks")
+        self.assertEqual( self.user.edit_category("Snacks","local foods"),True)
+        
+    def test_delete_category_not_found(self):
+        self.assertEqual( self.user.delete_category("not exist"), False)
+
+    def test_delete_category_deleted(self):
+        self.user.add_category("lunch recipes")
+        self.assertEqual( self.user.delete_category("lunch recipes"),
+                         True)
+        
+    
+class Recipe_categoryTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.recipes = Category("title")
+
+    
+    def test_add_recipe_added(self):
+        self.assertEqual(self.recipes.add_recipe("pillawo",'discription'), True)
+
+    def test_add_recipe_exists(self):
+        self.recipes.add_recipe("pillawo",'description')
+        self.assertEqual(self.recipes.add_recipe(
+            "pillawo",'description'), False)
+
+    def test_edit_recipe_not_found(self):
+        self.assertEqual(self.recipes.edit_recipe(
+            "chicken recipe", "beef recipe"), False)
+
+   
+    def test_edit_recipe_edited_succesfully(self):
+         self.recipes.add_recipe("pizza",'description')
+         self.assertEqual(self.recipes.edit_recipe("pizza", "chicken"), False)
+
+    def test_delete_recipe_not_found(self):
+        self.assertEqual(self.recipes.delete_recipe(
+        "katogo",'description'), False)
+
+    def test_recipe_deleted(self):
+        self.recipes.add_recipe("katogo",'description')
+        self.assertEqual(self.recipes.delete_recipe("katogo",'description'), True)
+
+class RecipeTest(unittest.TestCase):
+    def setUp(self):
+        self.recipe = Recipe("title","description")
+
+
+    def test_create_item_instance(self):
+        self.assertIsInstance(self.recipe, Recipe, "Failed to create instance")
